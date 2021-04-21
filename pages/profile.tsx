cc62d72c37b28437150d22729ee45c3741e6cf50
Ashutosh <crownashutosh01@gmail.com>
2021-04-21 22:03:54 +0530

    edited profile.tsx

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,47 +1,49 @@
 import BlogElement, { IBlogElement } from '../Components/BlogElement'
 import Meta from '../Components/Meta'
 import useSWR from 'swr'
-import { store } from '../store'
 import Router from 'next/router'
+import { GetServerSideProps, GetServerSidePropsContext } from 'next'
+import { getSession, session } from 'next-auth/client'
+import { FC } from 'react'
+import { Session } from 'next-auth'
+interface IProfile {
+  session: Session | null
+}
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  return {
+    props: {
+      session: await getSession(context)
+    }
+  }
+}
 
-const profile = () => {
-  const { userInfoState } = store.getState()
-  const { data } = useSWR('api/resources/fetch_blogs')
-  const blogs: IBlogElement[] = data
-
+const profile: FC<IProfile> = ({ session }) => {
   //--- Setting Authenticated state
 
   const isServer = () => typeof window === 'undefined'
   if (!isServer()) {
-    if (!userInfoState.authorised) {
+    if (!session) {
       Router.push('/login?referer=profile', '/login')
     }
-  }
-
-  if (blogs && typeof userInfoState.username === 'string') {
-    blogs.map(
-      (blog) =>
-        (blog.author = userInfoState.username ? userInfoState.username : '')
-    )
   }
 
   return (
     <div className='mx-10 my2'>
       <Meta
         title={`${
-          userInfoState.username !== undefined
-            ? userInfoState.username + ' | '
+          session?.user?.firstname !== undefined
+            ? session?.user?.firstname + ' | '
             : ''
         }Profile`}
       />
-      {!userInfoState.authorised ? null : (
+      {!session ? null : (
         <>
           <div className='info'>
-            {userInfoState.username !== undefined ? userInfoState.username : ''}
+            {session?.user?.firstname !== undefined
+              ? session?.user?.firstname
+              : ''}
           </div>
-          {!blogs && <div> No blogs so far...</div>}
-          {blogs &&
-            blogs.map((blog) => <BlogElement key={blog._id} {...blog} />)}
+          <div> No blogs so far...</div>
         </>
       )}
     </div>
